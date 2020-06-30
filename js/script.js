779bee61410f1e9695599f0d8b3b76ce7df1241a
Ilia Melnik <65551632+MelnikIlia@users.noreply.github.com>
2020-06-30 13:57:50 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,35 @@
+$('#log-in').click(function(){
+	$('.modal-register').css('display', 'flex').hide().fadeIn(200);
+	$('.form-login').css('display', 'flex');
+	$('body').css('overflow', 'hidden');
+});
+
+$('#sign-up').click(function(){
+	$('.modal-register').css('display', 'flex').hide().fadeIn(200);
+	$('.form-sign-up').css('display', 'flex');
+	$('body').css('overflow', 'hidden');
+});
+
+$('.modal-close').click(function(){
+	$('.modal-register').fadeOut(200);
+	$('.form-sign-up, .form-login').css('display', 'none');
+	$('body').css('overflow', 'auto');
+});
+
+(function ibg() {
+	$.each($(".ibg"), function (index, val) {
+		if ($(this).find("img").length > 0) {
+			$(this).css(
+				'background-image',
+				'url("' + $(this).find("img").attr("src") + '")'
+			);
+		} else if ($(this).find("picture").length > 0) {
+			$(this).css(
+				'background-image',
+				'url("' + $(this).find("source").attr("src") + '")'
+			);
+		}
+	});
+})();
+
+new WOW({mobile: false}).init();
